af40e4e2aa20a31fd6a81aebcd3995cbc544b2de
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-07 17:28:50 +0800

    feat(依赖): 添加json相关依赖

--- iot-framework/iot-common/build.gradle.kts
+++ iot-framework/iot-common/build.gradle.kts
@@ -3,4 +3,8 @@ dependencies {
     api("org.slf4j:slf4j-api")
     api("cn.hutool:hutool-all")
     api("com.alibaba:transmittable-thread-local")
+
+    api("com.fasterxml.jackson.core:jackson-databind")
+    api("com.fasterxml.jackson.core:jackson-core")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
