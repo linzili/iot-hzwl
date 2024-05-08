1ab4260cdd0bb10f034c23ccf5d6a4d03022225d
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-08 14:08:56 +0800

    feat(参数校验): 调整校验所有参数

--- iot-framework/iot-spring-boot-starter-web/src/main/kotlin/com/hzwl/framework/web/aspect/ValidatedAspect.kt
+++ iot-framework/iot-spring-boot-starter-web/src/main/kotlin/com/hzwl/framework/web/aspect/ValidatedAspect.kt
@@ -1,6 +1,6 @@
 package com.hzwl.framework.web.aspect
 
-import com.hzwl.framework.web.utils.AspectUtil
+import com.hzwl.framework.common.exception.ServiceException
 import jakarta.validation.Validator
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.stereotype.Component
 
 /**
- * 请求参数校验切面，统一对Controller中@RequestBody映射的对象进行校验，在Controller方法中无需单独处理
+ * 请求参数校验切面，统一对Controller中的参数进行校验，在Controller方法中无需单独处理
  */
 @Aspect
 @EnableAspectJAutoProxy
@@ -18,18 +18,29 @@ class ValidatedAspect(val validator: Validator) {
 
     @Around("execution(* com.hzwl..controller..*Controller.*(..))")
     fun around(proceedingJoinPoint: ProceedingJoinPoint): Any {
-        // 获取请求体对象
-        val body = AspectUtil.getBody(proceedingJoinPoint)
-        // 如果请求体对象不为空，则进行参数娇艳
-        if (body != null) {
-            // 进行校验
-            val validateResult = validator.validate(body)
+
+        proceedingJoinPoint.args.forEach { arg ->
+            // 校验参数
+            val validateResult = validator.validate(arg)
             if (validateResult.isNotEmpty()) {
                 // 校验未通过，抛出一场，响应状态码为400
                 val info = validateResult.joinToString(",") { it.message }
-                throw RuntimeException(info)
+                throw ServiceException(400, "请求参数不正确:$info")
             }
         }
+
+//        // 获取请求体对象
+//        val body = AspectUtil.getBody(proceedingJoinPoint)
+//        // 如果请求体对象不为空，则进行参数娇艳
+//        if (body != null) {
+//            // 进行校验
+//            val validateResult = validator.validate(body)
+//            if (validateResult.isNotEmpty()) {
+//                // 校验未通过，抛出一场，响应状态码为400
+//                val info = validateResult.joinToString(",") { it.message }
+//                throw RuntimeException(info)
+//            }
+//        }
         // 校验通过，执行原方法
         return proceedingJoinPoint.proceed(proceedingJoinPoint.args)
     }
