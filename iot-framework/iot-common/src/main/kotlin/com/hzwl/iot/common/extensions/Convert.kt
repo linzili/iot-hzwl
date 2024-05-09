9f30a58d2ff42aa183840a22fee28474111142d3
linzili <1983160303@qq.com>
2024-05-09 22:52:37 +0800

    feat(扩展): 新增convert功能、简化使用

--- iot-framework/iot-common/src/main/kotlin/com/hzwl/iot/common/extensions/Convert.kt
+++ iot-framework/iot-common/src/main/kotlin/com/hzwl/iot/common/extensions/Convert.kt
@@ -2,7 +2,33 @@ package com.hzwl.iot.common.extensions
 
 import cn.hutool.extra.spring.SpringUtil
 import io.github.linpeilie.Converter
+import io.github.linpeilie.CycleAvoidingMappingContext
 
 
-val <T : Any> T.convert: Converter
+val converter: Converter
     get() = SpringUtil.getBean(Converter::class.java)
+
+
+fun <S, R> convert(source: S, targetType: Class<R>): R {
+    return converter.convert(source, targetType)
+}
+
+fun <S, R> convert(source: S, target: R): R {
+    return converter.convert(source, target)
+}
+
+fun <S, R> convert(source: List<S>, targetType: Class<R>): List<R> {
+    return converter.convert(source, targetType)
+}
+
+fun <S, R> convert(source: S, targetType: Class<R>, context: CycleAvoidingMappingContext): R {
+    return converter.convert(source, targetType, context)
+}
+
+fun <S, R> convert(source: List<S>, targetType: Class<R>, context: CycleAvoidingMappingContext): List<R> {
+    return converter.convert(source, targetType, context)
+}
+
+fun <T> convert(map: Map<String, Any>, targetType: Class<T>): T {
+    return converter.convert(map, targetType)
+}
