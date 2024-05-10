e9f4270ed2d4482a4a7dff4532b125e747e7510c
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-10 16:04:43 +0800

    feat(工具): 日期工具类

---
+++ iot-framework/iot-common/src/main/kotlin/com/hzwl/iot/common/utils/date/DateUtils.kt
@@ -0,0 +1,11 @@
+package com.hzwl.iot.common.utils.date
+
+/**
+ * 时间工具类
+ *
+ * @constructor Create empty Date utils
+ */
+object DateUtils {
+
+    const val FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND = "yyyy-MM-dd HH:mm:ss"
+}
