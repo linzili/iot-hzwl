4560166d61df54cd4bda386c698e40269da01e23
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-10 17:31:34 +0800

    feat(mybatis): 多租户

--- iot-framework/iot-spring-boot-starter-mybatis/src/main/kotlin/com/hzwl/iot/framework/mybatis/config/MyBatisFlexConfiguration.kt
+++ iot-framework/iot-spring-boot-starter-mybatis/src/main/kotlin/com/hzwl/iot/framework/mybatis/config/MyBatisFlexConfiguration.kt
@@ -3,10 +3,12 @@ package com.hzwl.iot.framework.mybatis.config
 import com.hzwl.iot.common.extensions.log
 import com.hzwl.iot.framework.mybatis.core.entity.BaseEntity
 import com.hzwl.iot.framework.mybatis.core.handle.MyLogicDeleteProcessor
+import com.hzwl.iot.framework.mybatis.core.handle.MyTenantFactory
 import com.hzwl.iot.framework.mybatis.listener.CommentEventListener
 import com.mybatisflex.core.FlexGlobalConfig
 import com.mybatisflex.core.audit.AuditManager
 import com.mybatisflex.core.logicdelete.LogicDeleteProcessor
+import com.mybatisflex.core.tenant.TenantFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -29,6 +31,12 @@ class MyBatisFlexConfiguration {
         setLogicDelete()
 
         setListener()
+
+        setTenant()
+    }
+
+    private fun setTenant() {
+        config.tenantColumn = "tenant_id"
     }
 
     private final fun setListener() {
@@ -38,13 +46,17 @@ class MyBatisFlexConfiguration {
 
 
     private final fun setLogicDelete() {
-
         config.logicDeleteColumn = "deleted"
     }
 
     @Bean
     fun logicDeleteProcessor(): LogicDeleteProcessor {
         return MyLogicDeleteProcessor()
+    }
+
+    @Bean
+    fun tenantFactory(): TenantFactory {
+        return MyTenantFactory()
     }
 }
 
---
+++ iot-framework/iot-spring-boot-starter-mybatis/src/main/kotlin/com/hzwl/iot/framework/mybatis/core/handle/MyTenantFactory.kt
@@ -0,0 +1,19 @@
+package com.hzwl.iot.framework.mybatis.core.handle
+
+import com.mybatisflex.core.tenant.TenantFactory
+
+/**
+ * 多租户配置
+ * @author lin
+ */
+class MyTenantFactory : TenantFactory {
+
+
+    override fun getTenantIds(): Array<Any> {
+        return arrayOf(1)
+    }
+
+    override fun getTenantIds(tableName: String): Array<Any> {
+        return arrayOf(1)
+    }
+}
