8696729d04a89cffa29d6b891e1a40b57199d452
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-10 17:31:14 +0800

    test(device module): 添加测试类 及 配置文件

---
+++ iot-module-device/iot-module-device-biz/src/test/kotlin/com/hzwl/iot/DeviceModuleApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.hzwl.iot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DeviceModuleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
---
+++ iot-module-device/iot-module-device-biz/src/test/kotlin/com/hzwl/iot/product/ProductCategoryServiceTest.kt
@@ -0,0 +1,20 @@
+package com.hzwl.iot.product
+
+import com.hzwl.iot.module.device.service.product.ProductCategoryService
+import jakarta.annotation.Resource
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductCategoryServiceTest {
+
+    @Resource
+    lateinit var productCategoryService: ProductCategoryService
+
+
+    @Test
+    fun tenantTest() {
+
+        productCategoryService.mapper.selectAll()
+    }
+}
