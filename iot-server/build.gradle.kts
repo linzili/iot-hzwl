127ca896973f1d047875a5b18acd793f2491e4b8
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-11 17:56:50 +0800

    feat(插件): 添加打包插件

--- iot-server/build.gradle.kts
+++ iot-server/build.gradle.kts
@@ -1,5 +1,13 @@
+plugins {
+    id("application")
+}
 dependencies {
     implementation(project(":iot-module-system:iot-module-system-biz"))
+    implementation(project(":iot-module-device:iot-module-device-biz"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+application {
+    mainClass = "com.hzwl.iot.ServerApplicationKt"
 }
