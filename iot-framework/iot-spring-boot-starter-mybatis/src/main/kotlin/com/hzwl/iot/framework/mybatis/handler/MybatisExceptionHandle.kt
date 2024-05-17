c2227043e49e3ee53f202a3dc4d17ed56066ae0d
林子立 <9458419+lin-zili@user.noreply.gitee.com>
2024-05-17 16:46:27 +0800

    feat(异常处理): 添加MyBatisSystemException异常处理器

---
+++ iot-framework/iot-spring-boot-starter-mybatis/src/main/kotlin/com/hzwl/iot/framework/mybatis/handler/MybatisExceptionHandle.kt
@@ -0,0 +1,42 @@
+package com.hzwl.iot.framework.mybatis.handler
+
+import com.hzwl.iot.common.exception.ServiceException
+import com.hzwl.iot.common.exception.enums.GlobalErrorCodeConstants
+import com.hzwl.iot.common.pojo.R
+import org.apache.ibatis.builder.BuilderException
+import org.mybatis.spring.MyBatisSystemException
+import org.springframework.core.annotation.Order
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.reflect.InvocationTargetException
+
+/**
+ * 全局异常处理器，将 Exception 翻译成 R
+ * @author lin
+ */
+@RestControllerAdvice
+@Order(Integer.MIN_VALUE)
+class MybatisExceptionHandle {
+
+
+    /**
+     * 处理 Mybatis 系统异常
+     *
+     */
+    @ExceptionHandler(value = [MyBatisSystemException::class])
+    fun myBatisSystemExceptionHandler(ex: MyBatisSystemException): R<Nothing> {
+        (ex.cause as? BuilderException)?.cause
+            ?.takeIf { it is InvocationTargetException }
+            ?.let { it as InvocationTargetException }
+            ?.targetException
+            ?.takeIf { it is ServiceException }
+            ?.let {
+                it as ServiceException
+                return R.fail(it.code, it.message)
+            }
+
+        return R.fail(GlobalErrorCodeConstants.DATA_BASES_ERROR.code, GlobalErrorCodeConstants.DATA_BASES_ERROR.message)
+    }
+
+
+}
