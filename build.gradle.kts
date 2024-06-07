a4e73c67f2cb52255538a56b8432d9f848120090
linzili <1983160303@qq.com>
2024-06-07 17:10:40 +0800

    chore(依赖版本升级): mybatis-flex切换为ksp * mybatis-flex将kapt切换为ksp * 优化代码格式，增加可读性

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,13 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
     kotlin("kapt") version "2.0.0"
     kotlin("plugin.noarg") version "2.0.0"
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+
 }
 
 group = "com.hzwl"
@@ -20,7 +22,7 @@ repositories {
 
 
 ext {
-    set("mybatis-flex.version", "1.8.9")
+    set("mybatis-flex.version", "1.9.2")
     set("mybatis-flex-kotlin.version", "1.0.9")
     set("mapstruct-plus.version", "1.4.0")
     set("springdoc.version", "2.5.0")
@@ -37,15 +39,15 @@ subprojects {
         plugin("org.jetbrains.kotlin.plugin.spring")
         plugin("org.jetbrains.kotlin.plugin.noarg")
         plugin("org.jetbrains.kotlin.kapt")
+        plugin("com.google.devtools.ksp")
     }
     group = project.group
     version = project.version
-    kotlin {
-        sourceSets.all {
-            languageSettings {
-                languageVersion = "2.0"
-            }
-        }
+
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        mavenCentral()
     }
     noArg {
         annotation("com.mybatisflex.annotation.Table")
@@ -58,23 +60,25 @@ subprojects {
     }
     configurations.all {
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+        exclude("com.mybatis-flex", "mybatis-flex-processor")
     }
     configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
 
-    repositories {
-        mavenLocal()
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        mavenCentral()
-    }
+
     kotlin {
         compilerOptions {
             freeCompilerArgs.add("-Xjsr305=strict")
             freeCompilerArgs.add("-Xjvm-default=all")
-            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+//            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
             jvmTarget.set(JvmTarget.JVM_21)
+        }
+        sourceSets.all {
+            languageSettings {
+                languageVersion = "2.0"
+            }
         }
     }
 
@@ -90,7 +94,6 @@ subprojects {
             dependency("io.github.linpeilie:mapstruct-plus-processor:${rootProject.ext["mapstruct-plus.version"]}")
             dependency("com.mybatis-flex:mybatis-flex-spring-boot3-starter:${rootProject.ext["mybatis-flex.version"]}")
             dependency("com.mybatis-flex:mybatis-flex-core:${rootProject.ext["mybatis-flex.version"]}")
-            dependency("com.mybatis-flex:mybatis-flex-processor:${rootProject.ext["mybatis-flex.version"]}")
             dependency("com.mybatis-flex:mybatis-flex-kotlin-extensions:${rootProject.ext["mybatis-flex-kotlin.version"]}")
             dependency("org.springdoc:springdoc-openapi-starter-webmvc-ui:${rootProject.ext["springdoc.version"]}")
             dependency("cn.hutool:hutool-all:${rootProject.ext["hutool.version"]}")
@@ -100,7 +103,6 @@ subprojects {
         }
     }
     dependencies {
-        kapt("com.mybatis-flex:mybatis-flex-processor")
         kapt("io.github.linpeilie:mapstruct-plus-processor")
     }
 }
--- iot-framework/iot-spring-boot-starter-mybatis/build.gradle.kts
+++ iot-framework/iot-spring-boot-starter-mybatis/build.gradle.kts
@@ -4,9 +4,6 @@ dependencies {
     // mybatis-flex相关
     api("com.mybatis-flex:mybatis-flex-spring-boot3-starter")
     api("com.mybatis-flex:mybatis-flex-kotlin-extensions")
-    kapt("com.mybatis-flex:mybatis-flex-processor")
-
     runtimeOnly("com.mysql:mysql-connector-j")
-
     runtimeOnly("com.zaxxer:HikariCP")
 }
